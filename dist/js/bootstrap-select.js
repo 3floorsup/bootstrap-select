db187aa0778874edf83fee00161f20497f242ed2
Ashley Davison-White <adw999@gmail.com>
2016-04-01 11:23:34 +0200

    dist

--- dist/js/bootstrap-select.js
+++ dist/js/bootstrap-select.js
@@ -433,6 +433,7 @@
       this.autofocus = this.$element.prop('autofocus');
       this.$newElement = this.createView();
       this.$element
+		.wrap($('<div class="bootstrap-select-container"></div>'))
         .after(this.$newElement)
         .appendTo(this.$newElement);
       this.$button = this.$newElement.children('button');
